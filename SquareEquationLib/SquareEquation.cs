279975c5a3995042d8a38ca70a3839af88d3e3d1
Артём <82957545+OverCart345@users.noreply.github.com>
2023-06-24 16:40:47 +0600

    pls

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -6,31 +6,34 @@ public class SquareEquation
     {
         double eps = 1e-9;
 
-        if ((a > -eps && a < eps) || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c)) 
+        if ((a > -eps && a < eps) || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
         {
             throw new System.ArgumentException();
         }
 
-       
+
         double d = b * b - 4 * c;
-        double[] answer = new double[2];
+        double[] answer_zero = new double[2];
+        double[] answer_one = new double[1];
+        double[] answer_two = new double[2];
 
         if (d <= -eps)
         {
-             return answer;
+            return answer_zero;
         }
-        else if (-eps < d && d < eps) 
+        else if (-eps < d && d < eps)
         {
             double x1 = -b / (2 * a);
-            answer = new double[] { x1};
-            return answer;
+            answer_one[0] = x1;
+            return answer_one;
         }
         else
         {
             double x1 = (-b + Math.Sqrt(d)) / (2 * a);
             double x2 = (-b - Math.Sqrt(d)) / (2 * a);
-            answer = new double[] { x1, x2 };
-            return answer;
+            answer_two[0] = x1;
+            answer_two[1] = x2;
+            return answer_two;
         }
         
     }
